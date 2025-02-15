db0d93526d525c114c2335d2cf395bbd274a678b
vickygit702 <vigneshhari702@gmail.com>
2025-02-15 19:02:48 +0530

    files added

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { createRoot } from "react-dom/client";
+// import { Provider } from "react-redux";
+// import store from "./store";
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(<App />);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
