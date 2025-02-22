3a40c4304e340188747ab737d562090577e4b898
vickygit702 <vigneshhari702@gmail.com>
2025-02-22 19:53:31 +0530

    file modifications

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const { connectToDB } = require("./database/db");
 const authRoutes = require("./routes/Auth");
-const bookRoutes = require("./routes/userHome/Bookings/FindProvider");
-const userRoutes = require("./routes/userHome/FetchServices");
 const adminRoutes = require("./routes/admin/AdminPage");
 
 const server = express();
 
 server.use(express.json());
 server.use("/auth/api", authRoutes);
-server.use("/user", userRoutes);
-server.use("/booking/api", bookRoutes);
-
 server.use("/admin", adminRoutes);
 
 server.get("/", (req, res) => {
